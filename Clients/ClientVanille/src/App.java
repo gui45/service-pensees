7edb53e48cf2d5d50f445ec366da103e6c0dd9ff
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-04-03 09:34:08 -0400

    Test de lecture de xml sur url

--- Clients/ClientVanille/src/App.java
+++ Clients/ClientVanille/src/App.java
@@ -1,8 +1,23 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Scanner;
 
 public class App {
 
 	public static void main(String[] args) {
 
+		try {
+			URL urlListePensees = new URL("http://localhost/inspiration/src/pensee/liste/");
+			InputStream flux = urlListePensees.openConnection().getInputStream();
+			Scanner lecteur = new Scanner(flux);
+			lecteur.useDelimiter("\\A"); // delimiteur inexistant pour recuperer tout le flux
+			String xml = lecteur.next();
+			System.out.println(xml);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
