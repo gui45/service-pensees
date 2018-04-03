2dc898136bdd150306c95b562acab9344f54091a
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-04-03 08:58:48 -0400

    Base de données des pensees

---
+++ Service/src/accesseur/PenseeDAO.php
@@ -0,0 +1,16 @@
+<?php
+	include_once "basededonnees.php";
+	class PenseeDAO
+	{
+		function listerPensees()
+		{			
+		/*
+			$LISTER_PENSEES = "SELECT * FROM pensees";
+			global $basededonnees;
+			$requeteListerPensees = $basededonnees->prepare($LISTER_PENSEES);
+			$requeteListerPensees->execute();
+			return $requeteListerPensees->fetchAll();
+			*/
+		}
+	}
+?>
